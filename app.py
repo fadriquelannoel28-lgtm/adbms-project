7b15693ddf9612920960932c6870debf04275199
Fadriquelan Noel Andrei L <fadriquelannoel28@gmail.com>
2026-04-30 19:37:37 +0800

    added user login not yet complete

--- app.py
+++ app.py
@@ -1,34 +1,114 @@
-from flask import Flask, redirect, render_template
-
+from flask import Flask, redirect, render_template, request, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from models import User, db
+from functools import wraps
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Admin123@127.0.0.1:3306/medstock'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'medsave_key'
+
+db.init_app(app)
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 def index():
     return redirect('/Dashboard')
-@app.route('/Dashboard')
-def dashboard():
-    return render_template('index.html', page_title='Dashboard')
 
-@app.route('/Inventory-Management')
-def inventory_management():
-    return render_template('minventory.html', page_title='Inventory Management')
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/Dashboard/user-<int:user_id>/<name>')
+@login_required
+def dashboard(user_id, name): # Add these here
+    return render_template('index.html', page_title='Dashboard', u_id=user_id, u_name=name)
 
-@app.route('/Stock-Monitoring')
-def stock_monitoring():
-    return render_template('smonitoring.html', page_title='Stock Monitoring')
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/Inventory-Management/user-<int:user_id>/<name>')
+@login_required
+def inventory_management(user_id, name):
+    return render_template('minventory.html', page_title='Inventory Management', u_id=user_id, u_name=name)
 
-@app.route('/Ordering-System')
-def ordering_system():
-    return render_template('orderings.html', page_title='Ordering System')
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/Stock-Monitoring/user-<int:user_id>/<name>')
+@login_required
+def stock_monitoring(user_id, name):
+    return render_template('smonitoring.html', page_title='Stock Monitoring', u_id=user_id, u_name=name)
 
-@app.route('/Reports')
-def reports():
-    return render_template('reports.html', page_title='Reports')
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/Ordering-System/user-<int:user_id>/<name>')
+@login_required
+def ordering_system(user_id, name):
+    return render_template('orderings.html', page_title='Ordering System', u_id=user_id, u_name=name)
 
-@app.route('/User-Access')
-def user_access():
-    return render_template('user.html', page_title='User Access')
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/Reports/user-<int:user_id>/<name>')
+@login_required
+def reports(user_id, name):
+    return render_template('reports.html', page_title='Reports', u_id=user_id, u_name=name)
+
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/User-Access/user-<int:user_id>/<name>')
+@login_required
+def user_access(user_id, name):
+    return render_template('user.html', page_title='User Access', u_id=user_id, u_name=name)
+
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        new_user = User(
+            first_name = request.form.get('firstname'),
+            last_name = request.form.get('lastname'),
+            email = request.form.get('email'),
+            username = request.form.get('username'),
+            password_hash = generate_password_hash(request.form.get('password'))
+        )
+        try:
+            db.session.add(new_user)
+            db.session.commit() 
+            return redirect(url_for('login')) 
+        except Exception as e:
+            db.session.rollback()
+            return f"Error: {str(e)}"
+        
+    return render_template('register.html')
+
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        u_name = request.form.get('username')
+        u_pass = request.form.get('password')
+
+        user = User.query.filter_by(username=u_name).first()
+
+        if user and check_password_hash(user.password_hash, u_pass):
+
+            session['user_id'] = user.id
+            session['username'] = user.username
+            
+            return redirect(url_for('dashboard', user_id=user.id, name=user.username))
+        
+        return "Invalid Username or Password"
+
+    return render_template('login.html')
+
+# ------------------------------------------------------------------------------------------------------------------
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() 
     app.run(debug=True)
